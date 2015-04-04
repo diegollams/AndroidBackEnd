71734d4db619b8ffb1507cab4c23c346677f8814
Diego Llamas <diegollams@gmail.com>
2015-04-04 17:42:43 -0600

    changing to pg and seed

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150404231643) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "providers", force: :cascade do |t|
     t.string   "name"
     t.text     "direction"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Provider.create(name: 'Provedor del centro',direction: 'Calle norte',stars: 1.1,distance: 413.3)
+Provider.create(name: 'Provedor del norte',direction: 'Calle centro',stars: 5,distance: 413.3)
+Provider.create(name: 'Provedor del sur',direction: 'Calle sur',stars: 3.3,distance: 413.3)
+Provider.create(name: 'Provedor del juarez',direction: 'Calle este',stars: 4.6,distance: 413.3)
+Provider.create(name: 'Provedor del este',direction: 'Calle  otro gato',stars: 4.3,distance: 413.3)
+Provider.create(name: 'Provedor del barrio',direction: 'Calle barrio',stars: 3.3,distance: 413.3)
+Provider.create(name: 'Provedor del otro gato',direction: 'Calle madero',stars: 4.2,distance: 413.3)
+
+
+
+
